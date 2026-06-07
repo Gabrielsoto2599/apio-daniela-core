95d2e9fbd4b4ffdd15019d87e5aad5c6c626eebf
Gabrielsoto2599 <135544457+Gabrielsoto2599@users.noreply.github.com>
2026-06-07 13:10:48 -0400

    apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,10 +1,12 @@
 import { Platform } from 'react-native';
 
-// Si tu PC cambia de IP, solo cambias este valor aquí
-const PC_IP = '192.168.1.119'; 
-const PORT = '3001';
+// --- CONFIGURACIÓN DE PRODUCCIÓN (ACTIVA) ---
+export const BASE_URL = 'https://railway.app';
 
-export const BASE_URL = Platform.select({
-  web: 'http://127.0.0.1:' + PORT,
-  default: `http://${PC_IP}:${PORT}`, // Esto cubre Android e iOS
-});
+// --- CONFIGURACIÓN DE DESARROLLO LOCAL (COMENTADA) ---
+// const PC_IP = '192.168.1.119'; 
+// const PORT = '3001';
+// export const BASE_URL = Platform.select({
+//   web: 'http://127.0.0.1:' + PORT,
+//   default: `http://${PC_IP}:${PORT}`, 
+// });
