a1cf63f8ab5c5a4d914f269f574130c0b9a8a962
Gabrielsoto2599 <135544457+Gabrielsoto2599@users.noreply.github.com>
2026-06-07 12:39:22 -0400

    Update server.cjs

--- apio-conciencia/server.cjs
+++ apio-conciencia/server.cjs
@@ -38,11 +38,10 @@ app.use(express.json());
 // --- CONFIGURACIÓN DE CREDENCIALES FORZADA A PRODUCCIÓN ---
 const miApiKey = process.env.GEMINI_PRO || process.env.GEMINI_PRO_KEY || process.env.GOOGLE_API_KEY; 
 
-
-const embeddings = new GoogleGenerativeAIEmbeddings({ 
-    apiKey: miApiKey, 
-    modelName: "embedding-001" 
+const embeddings = new GoogleGenerativeAIEmbeddings({
+  apiKey: process.env.GEMINI_PRO_KEY
 });
+
 
 let vectorStore; // Variable global donde guardarás la memoria
 
