012b0ed39c1789ca9ad85a196990ebcf3d84fdf8
Gabrielsoto2599 <135544457+Gabrielsoto2599@users.noreply.github.com>
2026-06-08 13:49:43 -0400

    apiConfig.js

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,11 +1,11 @@
-// --- CONFIGURACIÓN DE PRODUCCIÓN (ACTIVA) ---
-export const BASE_URL ='https://daniela-ia-production-5356.up.railway.app';
+import { Platform } from 'react-native';
 
+// Cambia esta IP por la IP local real de tu computadora (la que tenías antes)
+const PC_IP = '192.168.1.119'; 
+const PORT = '3001';
 
-// --- CONFIGURACIÓN DE DESARROLLO LOCAL (COMENTADA) ---
-// const PC_IP = '192.168.1.119'; 
-// const PORT = '3001';
-// export const BASE_URL = Platform.select({
-//   web: 'http://127.0.0.1:' + PORT,
-//   default: `http://${PC_IP}:${PORT}`, 
-// });
+export const BASE_URL = Platform.select({
+  web: 'http://localhost:' + PORT,
+  android: 'http://10.0.2.2:' + PORT, // Ideal para el emulador de Android Studio
+  default: `http://${PC_IP}:${PORT}`,   // Ideal para el teléfono físico por Wi-Fi o iOS
+});
